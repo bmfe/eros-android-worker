1c3f857aa5910cac11c5c55cfbde4171230c7d57
limingyi <11111111>
2018-04-27 13:07:06 +0800

    mod:first commit

---
+++ app/src/main/java/com/benmu/wx/App.java
@@ -0,0 +1,23 @@
+package com.benmu.wx;
+
+import android.app.Application;
+
+import com.benmu.framework.BMInitConfig;
+import com.benmu.framework.BMWXApplication;
+import com.benmu.framework.BMWXEngine;
+import com.benmu.framework.constant.Constant;
+
+/**
+ * Created by Carry on 2017/8/23.
+ */
+
+public class App extends BMWXApplication {
+    public Application mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+}
